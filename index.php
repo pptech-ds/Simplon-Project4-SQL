dd2fb64477783ce6618727bab42336d8dc2c9ea5
pptech-ds <pptech.ds@gmail.com>
2021-05-31 10:10:05 +0200

    SQL connection etablished to WP database

--- index.php
+++ index.php
@@ -6,10 +6,16 @@ $pass = '';
 
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=pepinieres_baches_2', $user, $pass);
-    // foreach($dbh->query('SELECT * from FOO') as $row) {
-    //     print_r($row);
-    // }
-    // $dbh = null;
+
+    var_dump($dbh);
+
+    $tab = $dbh->query('SELECT * from wp_posts');
+
+    foreach($tab as $row) {
+         print_r($row);
+    }
+    $dbh = null;
+    
     echo 'Fin du SQL!';
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
