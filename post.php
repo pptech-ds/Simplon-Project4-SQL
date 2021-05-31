794a63fdcfa2595ffd2553faedb83b692939db18
pptech-ds <pptech.ds@gmail.com>
2021-05-31 17:13:58 +0200

    new page post.php with unique post added

--- post.php
+++ post.php
@@ -21,26 +21,25 @@ try {
     die();
 }
 
+if (isset($_GET['id'])) {
 
+    $post_id = $_GET['id'];
 
-try {
-    $query = '  SELECT 
-                    wp_posts.ID,
-                    post_title, 
-                    post_content,
-                    LEFT(post_content, 100) AS post_content_tr, 
-                    post_date, 
-                    display_name  
-                FROM wp_posts, wp_users
-                WHERE post_type = "post"
-                    AND post_status = "publish"
-                    AND post_author = wp_users.ID
-                    ORDER BY post_date DESC';
+    try {
+        $query = "  SELECT 
+                        wp_posts.ID,
+                        post_title, 
+                        post_content,
+                        LEFT(post_content, 100) AS post_content_tr, 
+                        post_date, 
+                        display_name  
+                    FROM wp_posts, wp_users
+                    WHERE wp_posts.ID = $post_id";
 
-    $req = $dbh->query($query);
-    $req ->setFetchMode(PDO::FETCH_ASSOC);
-    $tab = $req->fetchAll();
-    $req ->closeCursor();
+        $req = $dbh->query($query);
+        $req ->setFetchMode(PDO::FETCH_ASSOC);
+        $tab = $req->fetchAll();
+        $req ->closeCursor();
 ?>
 
     
@@ -50,12 +49,12 @@ try {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>SQL first steps</title>
+        <title>SQL first steps - Post</title>
     </head>
     <body>
-        <h1>My SQL first steps</h1>
+        <h1>Post Page</h1>
 
-<?php
+        <?php
     foreach($tab as $row) {
     
 ?>
@@ -63,24 +62,27 @@ try {
         <h2><a href="post.php?id=<?= $row['ID'] ?>"><?= $row['post_title'] ?></a></h2>
         <p><?= $row['post_content'] ?></p>
         <p>Writtent by: <?= $row['display_name'] ?> - Date: <?= $row['post_date'] ?></p>
+
+        <h2><a href="index.php">Get Back to main page</a></h2>
       
 
     </body>
     </html>
 
 <?php
-    }
+        }
     // var_dump($tab);
 
     // foreach($tab as $row) {
     //     // print_r($row);
     //     var_dump($row);
     // }
-    $dbh = null;
+        $dbh = null;
     
     // echo 'End of Request !';
-} catch (PDOException $e) {
-    print "Error !: " . $e->getMessage() . "<br/>";
-    die();
+    } catch (PDOException $e) {
+        print "Error !: " . $e->getMessage() . "<br/>";
+        die();
+    }
 }
 ?>
