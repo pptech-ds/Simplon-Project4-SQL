6c1d4d411305aff13300585d082e80903fa6cd0d
pptech-ds <pptech.ds@gmail.com>
2021-05-31 12:31:04 +0200

    SQL requests code optimized

--- index.php
+++ index.php
@@ -2,17 +2,37 @@
 
 $user = 'root';
 $pass = '';
+$dbname = 'pepinieres_baches_2';
+$host = 'localhost';
 
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=pepinieres_baches_2', $user, $pass);
-
+    $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+    // echo $dsn;
+    $options = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+    $dbh = new PDO($dsn, $user, $pass, $options);
     var_dump($dbh);
+    echo 'Connexion etablished !';
+} catch (PDOException $e) {
+    print "Error !: " . $e->getMessage() . "<br/>";
+    die();
+}
 
-    $tab = $dbh->query('SELECT post_title, LEFT(post_content, 100), post_date  
-                        FROM wp_posts
+
+
+try {
+    $tab = $dbh->query('SELECT 
+                            post_title, 
+                            LEFT(post_content, 100), 
+                            post_date, 
+                            display_name  
+                        FROM wp_posts, wp_users
                         WHERE post_type = "post"
-                        AND post_status = "publish"');
+                            AND post_status = "publish"
+                            AND post_author = wp_users.ID');
     var_dump($tab);
 
     foreach($tab as $row) {
@@ -21,9 +41,9 @@ try {
     }
     $dbh = null;
     
-    echo 'Fin du SQL!';
+    echo 'End of Request !';
 } catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
+    print "Error !: " . $e->getMessage() . "<br/>";
     die();
 }
 ?>
